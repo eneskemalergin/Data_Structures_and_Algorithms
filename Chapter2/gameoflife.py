00566438b099ccbcf3a064b07c63ed7c063da9b0
Enes Kemal Ergin <eneskemalergin@gmail.com>
2016-11-09 19:08:34 -0600

    random initialization added for game of life

--- Chapter2/gameoflife.py
+++ Chapter2/gameoflife.py
@@ -1,8 +1,10 @@
 # Main program that plays the game of life.
+import random
 from life import LifeGrid
 
+
 # # Define the initial configuration of live cells.
-INIT_CONFIG = [(1,1), (1,2), (2,2), (3,2)]
+# INIT_CONFIG = [(1,1), (1,2), (2,2), (3,2)]
 #
 # # Set the size of the grid.
 # GRID_WIDTH = 5
@@ -17,6 +19,8 @@ def main():
     GRID_HEIGHT = int(input("Enter height for the grid: "))
     NUM_GENS = int(input("Enter number of generations: "))
 
+    INIT_CONFIG = randomInitial(GRID_WIDTH, GRID_HEIGHT)
+    print(INIT_CONFIG)
     grid = LifeGrid(GRID_WIDTH, GRID_HEIGHT)
     grid.configure(INIT_CONFIG)
 
@@ -32,8 +36,10 @@ def evolve(grid):
     liveCells = list()
 
     # Iterate over the elements of the grid.
-    for i in range(grid.numRows()):
-        for j in range(grid.numCols()):
+    for i in range(grid.numRows()-1):
+        # print i
+        for j in range(grid.numCols()-1):
+            # print j
             # Determine the number of live neighbors for this cells
             neighbors = grid.numLiveNeighbors(i,j)
 
@@ -50,12 +56,27 @@ def draw(grid):
     for row in range(grid.numRows()):
         for col in range(grid.numCols()):
             if grid.isLiveCell(row, col):
-                print '1 ',
+                print '@ ',
             else:
-                print '0 ',
+                print '. ',
         print ''
     print ''
 
+
+# Creates a random intial configuration
+def randomInitial(GRID_WIDTH, GRID_HEIGHT):
+    """
+    numOfCells -> 4 Constant
+    GRID_WIDTH -> input (int)
+    GRID_HEIGHT -> input (int)
+
+    """
+    numOfCells = 4
+    new_config = []
+    for _ in range(numOfCells):
+        temp_tuple = ( random.randrange(0, (GRID_WIDTH - 1)) , random.randrange( 0,(GRID_HEIGHT - 1)) )
+        new_config.append(temp_tuple)
+    return new_config
 
 # Executes the main routine.
 if __name__ == '__main__':
