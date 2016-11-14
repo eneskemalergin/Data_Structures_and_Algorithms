48dc38a62dd6c1e6deea5f8052f65cf318f31ddf
Enes Kemal Ergin <eneskemalergin@gmail.com>
2016-11-13 21:28:26 -0600

    binary set implementation added

--- Chapter5/README.md
+++ Chapter5/README.md
@@ -2,16 +2,16 @@
 
 ### To Do List:
 
-- [ ] Finish chapter notes in ipynb
-- [ ] Implement linear search algorithms with cases
-- [ ] Implement binary search algorithm
-- [ ] Implement Bubble sort algorithm
-- [ ] Implement selection sort algorithm
-- [ ] Implement insertion sort algorithm
-- [ ] Working with Sorted Lists
-- [ ] Maintaining a Sorted List
-- [ ] Merging Sorted Lists
-- [ ] Define and Implement Set ADT using sorted list
+- [x] Finish chapter notes in ipynb
+- [x] Implement linear search algorithms with cases
+- [x] Implement binary search algorithm
+- [x] Implement Bubble sort algorithm
+- [x] Implement selection sort algorithm
+- [x] Implement insertion sort algorithm
+- [x] Working with Sorted Lists
+- [x] Maintaining a Sorted List
+- [x] Merging Sorted Lists
+- [x] Define and Implement Set ADT using sorted list
 - [ ] Modify and make necessary addition to Set ADT
 - [ ] Write a small snippet to verify usage of Set ADT
 - [ ] Define and Implement Bag ADT from chapter 1 again with sorted list and the binary search algorithm
---
+++ Chapter5/binaryset.py
@@ -0,0 +1,114 @@
+# Implementation of the Set ADT using a sorted list.
+
+class Set:
+    # Creates an empty set instance.
+    def __init__(self):
+        self._theElements = list()
+
+    # Returns the number of items in the set.
+    def __len__(self):
+        return len(self._theElements)
+
+    # Determines if an element is in the set.
+    def __contains__(self, element):
+        ndx = self._findPosition(element)
+        return ndx < len(self) and self._theElements[ndx] == element
+
+    # Adds a new unique element to the set
+    def add(self, element):
+        if element not in self:
+            ndx = self._findPosition(element)
+            self._theElements.insert(ndx, element)
+
+    # Removes an element from the set.
+    def remove(self, element):
+        assert element in self, "The element must be in the set."
+        ndx = self._findPosition(element)
+        self._theElements.pop(ndx)
+
+    # Determines if this set is a subset of setB
+    def isSubsetOf(self, setB):
+        for element in self:
+            if element not in setB:
+                return False
+        return True
+
+    def __eq__(self, setB):
+        if len(self) != len(setB):
+            return False
+        else:
+            for i in range(len(self)):
+                if self._theElements[i] != setB._theElements[i]:
+                    return False
+            return True
+
+    def union(self, setB):
+        newSet = Set()
+        a = 0
+        b = 0
+        # Merge the two lists together until one is empty.
+        while a < len(self) and b < len(setB):
+            valueA = self._theElements[a]
+            valueb = setB._theElements[b]
+
+            if valueA < valueB:
+                newSet._theElements.append(valueA)
+                a += 1
+            elif valueA > valueB:
+                newSet._theElements.append(valueB)
+                b += 1
+            # Only one of the two duplicates are appended
+            else:
+                newSet._theElements.append(valueA)
+                a += 1
+                b += 1
+
+        # If a listA contains more items, append them to newList.
+        while a < len(self):
+            newSet._theElements.append(self._theElements[a])
+            a += 1
+
+        # Or if listB contains more items, append them to newList.
+        while b < len(setB):
+            newSet._theElements.append(setB._theElements[b])
+            b += 1
+
+        return newSet
+
+
+
+    # The remaining mehtods go here.
+
+    def __iter__(self):
+        return _SetIterator(self._theElements)
+
+    def _findPosition(self, element):
+        low = 0
+        high = len(theList) - 1
+        while low <= high:
+            mid = (high - low) / 2
+            if theList[mid] == target:
+                return mid
+            elif target < theList[mid]:
+                high = mid - 1
+            else:
+                low = mid + 1
+
+        return low
+
+# Iterator class for set __iter__
+class _SetIterator:
+    def __init__( self, theList ):
+        self._setItems = theList
+        self._curNdx = 0
+
+    def __iter__( self ):
+        return self
+
+    def next( self ):
+        if self._curNdx < len( self._setItems ):
+            item = self._setItems[ self._curNdx ]
+            self._curNdx += 1
+            return item
+        else:
+            raise StopIteration
